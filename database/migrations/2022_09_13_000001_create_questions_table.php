898dc8688235016715526feb71a326f9122e38cc
NodjimbaMsa <85159482+NodjimbaMsa@users.noreply.github.com>
2022-09-14 10:04:26 +0200

    Init migrations

---
+++ database/migrations/2022_09_13_000001_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('order');
+            $table->string('title');
+            $table->integer('type');
+            $table->string('slug ');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
