1540d4c75f306bb89a909fc8bb8ab8733b0f2266
sebastianmel <57873668+sebastianmel@users.noreply.github.com>
2022-09-16 10:28:02 +0200

    data update 2
    recuperation : of migrations , seeders ,models and factories (data the last repository)

---
+++ database/factories/ResponseFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Response;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ResponseFactory extends Factory
+{
+    protected $model = Response::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'response_text' => $this->faker->word,
+        ];
+       
+    }
+}
