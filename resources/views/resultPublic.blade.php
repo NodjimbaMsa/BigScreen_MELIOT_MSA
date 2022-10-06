d76a4047e71c26c04d479385e2ae2c6b9e0c4ad7
sebastianmel <57873668+sebastianmel@users.noreply.github.com>
2022-10-06 11:03:41 +0200

    .

--- resources/views/resultPublic.blade.php
+++ resources/views/resultPublic.blade.php
@@ -8,115 +8,25 @@
     font-family: 'Nunito', sans-serif;
 } */
 
-a{
-    color: whitesmoke;
-}
-#all{
-    border: none;
-    display: flex;
-    border-radius: 20px;
-    width: 90%;
-    margin: auto;
-    box-shadow: 0px 1px 3px 0px #fff,
-            1px 2px 4px 0px #fff,
-            2px 4px 8px 0px #fff,
-            2px 4px 16px 0px #fff;
-}
-#buttonAll{
-    padding: 80px;
-    text-align: center;
-}
-   
-
-
-
-#center{
-   
-    position: relative;
-    background-color: #000000;
-    height: 80vh;
-    width: 100%;
-    border-radius: 20px;
-    
-    
-}
-
-
-
-
-/* Button */
-
-
-.btn-neon{
-    text-decoration: none;
-    text-transform: uppercase;
-    text-align: center;
-    font-size: 14px;
-    line-height: 50px;
-    color: #f5f5f5;
-    width: 180px;
-    transition: 1s;
-    transition-delay: .9s;
-    position: relative;
-    display: inline-block;
-    border-radius: 50px;
-}
-
-.btn-neon:hover{
-    box-shadow: 0 0 10px #fdee00,
-                0 0 40px #fdee00,
-                0 0 80px #fdee00;
-    background-color: #fdee00;
-    
-}
-
-.btn-neon:hover polyline{
-    stroke-dashoffset: -460;
-    
-}
-
-.btn-neon svg{
-    position: absolute;
-    top:0;
-    left:0;
-    right:0;
-    bottom:0;
-    width: 100%;
-    height: 100%;
-    border-radius: 50px;
-    
-}
-
-.btn-neon svg polyline{
-    fill: transparent;
-    stroke: #fdee00;
-    stroke-width: 2px;
-    stroke-dasharray: 40 460;
-    stroke-dashoffset: 40;
-    transition:  .8s ease-in-out;
-    
-}
+  .bg-light {
+    color: black;
+  }
 </style>
-<p class="lead">Results</p>
-<div class="resultP">
+<div class="bg-light  p-5 rounded mt-3">
 
-    <!-- CENTRAL BIG WINDOWS -->
-    <section id="all">
-        <!-- Part on the left -->
-        <div id="center">
-           
-            <div id="buttonAll">
-
-              <a href="#" class="btn-neon">
-                Print
-                <svg height="50" width="180">
-                  <polyline points="0,0 180,0 180,50 0,50 0,00">
-                  </polyline>
-                </svg>
-              </a>
-            </div>
-
-        </div>
-    </section>
+  <h2>Question 1/20</h2>
+  <div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label">Votre adresse mail :</label>
+    <div class="points">
+      <input type="email" value="mike@gmail.com" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+  </div>
+  <h2>Question 2/20</h2>
+  <div class="mb-3">
+    <label for="exampleFormControlTextarea1" class="form-label">Votre âge :</label>
+    <div class="points">
+    <input type="number" class="form-control" id="exampleFormControlInput2" placeholder="18 ans">
+    </div>
+  </div>
 </div>
 @endsection('content')
